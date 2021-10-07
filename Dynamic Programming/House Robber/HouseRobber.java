6b76da29228f3d0a14eaddbc44bd049389999922
diganta413 <ghoshdiganta226@gmail.com>
2021-10-07 23:39:11 +0530

    Starting all over again

---
+++ Dynamic Programming/House Robber/HouseRobber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class HouseRobber{
+    int maxValue(ArrayList<Integer> houses,int currentIndex){
+        int i;
+        if(currentIndex > houses.size()){
+            return 0;
+        }
+        else
+        {
+            for(i=houses.size()-1;i>=0;i--){
+                int max_with_cur = houses.get();
+        }
+    }
+    }
+}
